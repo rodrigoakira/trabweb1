96a6e59662e2bda3f64f0fadb2722791dcf53f66
rodrigoakira <akira.tkhs@gmail.com>
2018-05-23 21:40:41 -0300

    commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body class = "back-page">    
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
+    
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
@@ -19,16 +19,15 @@
       <a class="navbar-brand" href="pages/sobre.php"><i class="glyphicon glyphicon-user"></i> Rodrigo Takahashi - Gestor Adm e Analista de Sistemas</a>
     </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
+    
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav navbar-right">
         <li><a href="pages/curriculo.php"><i class="glyphicon glyphicon-list-alt"></i> Meu Curriculo</a></li>
         <li><a href="pages/contato.php"><i class="glyphicon glyphicon-envelope"></i> Contato</a></li>
               </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+    </div>
+  </div>
 </nav>
-
 
 <footer class="main-footer footer-landpage">
 <div class="pull-right hidden-xs">
